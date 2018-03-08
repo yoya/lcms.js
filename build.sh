c5dd70009c5b9e992a67c16456eabb118848aa22
Yoshihiro Yamazaki <yoya@awm.jp>
2018-03-09 02:32:41 +0900

    rm -O2, -O3

--- build.sh
+++ build.sh
@@ -12,7 +12,8 @@ done
 $EMCC -o $BIN_DIR/lcms.js -I $INCLUDE_DIR $SRC_DIR/*.c -s EXPORTED_FUNCTIONS=["$exported_opt"] -s ASSERTIONS=1
 
 $EMCC -o $BIN_DIR/lcms-O1.js -I $INCLUDE_DIR $SRC_DIR/*.c -s EXPORTED_FUNCTIONS=["$exported_opt"] -O1 -s ASSERTIONS=1
-$EMCC -o $BIN_DIR/lcms-O2.js -I $INCLUDE_DIR $SRC_DIR/*.c -s EXPORTED_FUNCTIONS=["$exported_opt"] -O2 -s ASSERTIONS=1
-$EMCC -o $BIN_DIR/lcms-O3.js -I $INCLUDE_DIR $SRC_DIR/*.c -s EXPORTED_FUNCTIONS=["$exported_opt"] -O3 -s ASSERTIONS=1
+
+# $EMCC -o $BIN_DIR/lcms-O2.js -I $INCLUDE_DIR $SRC_DIR/*.c -s EXPORTED_FUNCTIONS=["$exported_opt"] -O2 -s ASSERTIONS=1
+# $EMCC -o $BIN_DIR/lcms-O3.js -I $INCLUDE_DIR $SRC_DIR/*.c -s EXPORTED_FUNCTIONS=["$exported_opt"] -O3 -s ASSERTIONS=1
 
 $EMCC -o $BIN_DIR/lcms-all.js -I $INCLUDE_DIR $SRC_DIR/*.c -s EXPORT_ALL=1 -s ASSERTIONS=1
