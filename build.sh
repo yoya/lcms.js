52f1b412f56d9319570e4dffa90eb0be5adc80dc
Yoshihiro Yamazaki <yoya@awm.jp>
2018-03-11 15:57:59 +0900

    total memory 16777216 to 33554432

--- build.sh
+++ build.sh
@@ -9,11 +9,11 @@ for s in $SYMBOLS
 do exported_opt="\"_$s\",$exported_opt"
 done
 
-$EMCC -o $BIN_DIR/lcms.js -I $INCLUDE_DIR $SRC_DIR/*.c -s EXPORTED_FUNCTIONS=["$exported_opt"] -s ASSERTIONS=1
+$EMCC -o $BIN_DIR/lcms.js -I $INCLUDE_DIR $SRC_DIR/*.c -s EXPORTED_FUNCTIONS=["$exported_opt"] -s ASSERTIONS=1 -s TOTAL_MEMORY=33554432
 
-$EMCC -o $BIN_DIR/lcms-O1.js -I $INCLUDE_DIR $SRC_DIR/*.c -s EXPORTED_FUNCTIONS=["$exported_opt"] -O1 -s ASSERTIONS=1
+$EMCC -o $BIN_DIR/lcms-O1.js -I $INCLUDE_DIR $SRC_DIR/*.c -s EXPORTED_FUNCTIONS=["$exported_opt"] -s ASSERTIONS=1 -s TOTAL_MEMORY=33554432
 
 # $EMCC -o $BIN_DIR/lcms-O2.js -I $INCLUDE_DIR $SRC_DIR/*.c -s EXPORTED_FUNCTIONS=["$exported_opt"] -O2 -s ASSERTIONS=1
 # $EMCC -o $BIN_DIR/lcms-O3.js -I $INCLUDE_DIR $SRC_DIR/*.c -s EXPORTED_FUNCTIONS=["$exported_opt"] -O3 -s ASSERTIONS=1
 
-$EMCC -o $BIN_DIR/lcms-all.js -I $INCLUDE_DIR $SRC_DIR/*.c -s EXPORT_ALL=1 -s ASSERTIONS=1
+$EMCC -o $BIN_DIR/lcms-all.js -I $INCLUDE_DIR $SRC_DIR/*.c -s EXPORT_ALL=1 -s ASSERTIONS=1 -s TOTAL_MEMORY=33554432
